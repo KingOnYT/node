35358e8ee4e1ef7cfe3939f824384cfdf40ea26e
KingOnYT <87886576+KingOnYT@users.noreply.github.com>
2021-10-17 17:28:45 -0700

    Add changes CSS

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
-var err = "404";
 const app = express();
+//connection
+const dbURL =
+  "mongodb+srv://Kevin:KK711711@nodoblogs.gwy1e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const morgan = require("morgan");
+
 const blogs = [
   {
     title: "How Programming Benefits you",
@@ -17,24 +22,43 @@ const blogs = [
 ];
 
 app.set("view engine", "ejs");
-
+app.use(express.static("public"));
 app.listen(3000);
-console.log("working");
+function running() {
+  console.log("Node Blogs ");
+  console.log("Local host Server ");
+  console.log("------------------------");
+  console.log("Server Running on Localhost:3000");
+}
+running();
+// Logging info to console
+app.use(morgan("dev"));
+//home
 app.get("/", (req, res) => {
-  //   res.send("<h1>Hi</h1>");
+  res.status(200);
+
   res.render("index", { title: "Home", blogs });
 });
-
+//about
 app.get("/about", (req, res) => {
-  //   res.send("<h1>about</h1>");
+  res.status(200);
+
   res.render("about", { title: "About" });
 });
+//blogs create
 app.get("/blogs/create", (req, res) => {
+  res.status(200);
+
   res.render("create", { title: "Create" });
+});
+//blogs
+app.get("/blogs", (req, res) => {
+  res.status(200);
+
+  res.render("blogs", { title: "blogs", blogs });
 });
 // 404 no founid
 app.use((req, res) => {
-  res.render("404", { title: err });
-
-  //   res.send("<h1>main</h1>");
+  res.status(404);
+  res.render("404", { title: "404" });
 });
